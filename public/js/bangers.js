f9d903840c5f5120a8a66febc29d03d5d4ea9565
Rafael <rafael@rafael-laptop.(none)>
2013-03-02 10:43:29 +0000

    Página secundaria a meio

---
+++ public/js/bangers.js
@@ -0,0 +1,16 @@
+function Bangers($scope) {
+  $scope.bangers= [
+    [{"name": "1 lady",
+     "img": "images/high_tatras_min.jpg"},
+    {"name": "2 lady",
+     "img": "images/high_tatras2_min.jpg"},
+    {"name": "3 lady",
+     "img": "images/bang.jpg"},
+     {"name": "4 lady",
+     "img": "-"},
+     {"name": "5 lady",
+     "img": "-"}],
+     [{"name": "6 lady",
+     "img": "-"}]
+  ];
+}
